abb72c898aada0c74838dfb5dc3e0b7e6b6f08b0
amir <amir.ict.engg@gmail.com>
2022-10-02 09:35:24 +0600

    bla

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/.html"],
+    content: ['./src/*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+
+            colors: {
+                darkcyan: 'hsl(158, 36%, 37%)',
+                cream: 'hsl(30, 38%, 92%)',
+                verydarkblue: 'hsl(212, 21%, 14%)',
+                darkgrayishblue: 'hsl( 228, 12%, 48%)',
+                white: 'hsl(0, 0%, 100%)'
+            },
+            fontFamily: {
+                fraunces: ['Fraunces', ],
+                montserrat: ['Montserrat']
+            }
+        },
     },
     plugins: [],
 }
